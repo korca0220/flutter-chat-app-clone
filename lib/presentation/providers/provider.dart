a77e1b916a797ffb06e8e092566c8b1f935e7301
ParkJuneWoo <krca0220@gmail.com>
2023-04-22 21:36:33 +0900

    Add AuthProvider

--- lib/presentation/providers/provider.dart
+++ lib/presentation/providers/provider.dart
@@ -1,4 +1,2 @@
-export 'global/auth_provider.dart';
-export 'global/firebase_auth_provider.dart';
-export 'repository/auth_repository_provider.dart';
-export 'use_case/auth_use_case_provider.dart';
+export 'auth_provider.dart';
+export 'di/di.dart';
